b0ae83631237f8d28bb4786452b98203d44c8d0d
PriyankaMannikeri <80226560+PriyankaMannikeri@users.noreply.github.com>
2021-03-29 03:30:20 -0700

    add percentile filtering

--- process_data.py
+++ process_data.py
@@ -23,6 +23,22 @@ vechicles_csv = pd.read_csv(vechicles_csv_processed_path)
 print(f"total number of rows in cleaned csv (no rows contain nan): {len(vechicles_csv)}")
 
 
+# Removing outliers
+odometer_low = vechicles_csv.odometer.quantile(.01)
+odometer_high = vechicles_csv.odometer.quantile(.95)
+print(odometer_low, odometer_high)
+# vechicles_csv.odometer = vechicles_csv.odometer.clip(lower=odometer_low, upper=odometer_high)
+vechicles_csv = vechicles_csv[vechicles_csv.odometer < odometer_high]
+vechicles_csv = vechicles_csv[vechicles_csv.odometer > odometer_low]
+
+price_low = vechicles_csv.price.quantile(.045)
+price_high = vechicles_csv.price.quantile(.988)
+print(price_low, price_high)
+# vechicles_csv.price = vechicles_csv.price.clip(lower=price_low, upper=price_high)
+vechicles_csv = vechicles_csv[vechicles_csv.price < price_high]
+vechicles_csv = vechicles_csv[vechicles_csv.price > price_low]
+
+print(f"total number of rows in cleaned csv (no rows contain nan): {len(vechicles_csv)}")
 object_cat_columns = ["region", "manufacturer", "model", "condition", "cylinders", "fuel", "title_status", "transmission",
                       "drive", "type", "paint_color", "state"]
 df, encoding_dict = label_encoding(vechicles_csv, object_cat_columns)
@@ -33,6 +49,6 @@ df_numpy = df.to_numpy()
 # print(df_numpy.shape)
 df_numpy = np.delete(df_numpy, 0, 1)
 df_numpy[:, [1, 14]] = df_numpy[:, [14, 1]]
-print(df_numpy.shape)
+# print(df_numpy.shape)
 #
 np.save(open("features.npy", "wb"), df_numpy)
