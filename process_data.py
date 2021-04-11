0f2b977882c18dc27d6e45aec3976e5ec5fcc6c6
PriyankaMannikeri <80226560+PriyankaMannikeri@users.noreply.github.com>
2021-04-10 20:46:17 -0700

    filter data

--- process_data.py
+++ process_data.py
@@ -7,8 +7,8 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 
-vechicles_csv = pd.read_csv("vehicles.csv")
-print(f"total number of rows in original csv: {len(vechicles_csv)}")
+# vechicles_csv = pd.read_csv("vehicles.csv")
+# print(f"total number of rows in original csv: {len(vechicles_csv)}")
 
 # input path where the new cleaned csv will be saved
 vechicles_csv_processed_path = "vehicles-cleaned.csv"
@@ -17,23 +17,29 @@ features_to_consider = ["region", "price", "year", "manufacturer", "model", "con
                         "title_status", "transmission", "drive", "type", "paint_color", "state"]
 
 # # drop rows that contain atleast one na value
-vechicles_csv = vechicles_csv.dropna(subset=features_to_consider)
-colms_to_remove = [col for col in vechicles_csv.columns if col not in features_to_consider]
-vechicles_csv.drop(colms_to_remove, axis=1, inplace=True)
-vechicles_csv.to_csv(vechicles_csv_processed_path)
+# vechicles_csv = vechicles_csv.dropna(subset=features_to_consider)
+# colms_to_remove = [col for col in vechicles_csv.columns if col not in features_to_consider]
+# vechicles_csv.drop(colms_to_remove, axis=1, inplace=True)
+# vechicles_csv.to_csv(vechicles_csv_processed_path)
 # print(f"total number of rows in cleaned csv (no rows contain nan): {len(vechicles_csv)}")
 
 vechicles_csv = pd.read_csv(vechicles_csv_processed_path)
 print(f"total number of rows in cleaned csv (no rows contain nan): {len(vechicles_csv)}")
 
+# filter year, consider after 1995
+# vechicles_csv = vechicles_csv[vechicles_csv["year"].between(1995, 2021)]
+vechicles_csv = vechicles_csv[vechicles_csv.year >= 2000]
+
 # Removing outliers
-odometer_low = vechicles_csv.odometer.quantile(.01)
+# odometer_low = vechicles_csv.odometer.quantile(.01)
+odometer_low = 4000
 odometer_high = vechicles_csv.odometer.quantile(.95)
 print(odometer_low, odometer_high)
 vechicles_csv = vechicles_csv[vechicles_csv.odometer < odometer_high]
 vechicles_csv = vechicles_csv[vechicles_csv.odometer > odometer_low]
 
-price_low = vechicles_csv.price.quantile(.045)
+# price_low = vechicles_csv.price.quantile(.045)
+price_low = 1000
 price_high = vechicles_csv.price.quantile(.988)
 print(price_low, price_high)
 vechicles_csv = vechicles_csv[vechicles_csv.price < price_high]
@@ -59,19 +65,20 @@ vechicles_csv.drop_duplicates(inplace=True)
 vechicles_csv.reset_index(drop=True, inplace=True)
 vechicles_csv.reset_index(drop=True)
 print(f"total number of rows in cleaned csv (after removing duplicates): {len(vechicles_csv)}")
-
+exit(-1)
+price_df = vechicles_csv.pop("price")
+vechicles_csv["price"] = price_df
 # correlation
 corrMatrix = vechicles_csv.corr()
 fig = plt.figure(figsize=(15, 15))
 sn.heatmap(corrMatrix, annot=True)
 plt.title("Pearson Correlation")
 # plt.show()
-plt.savefig("./heatmap-correlation.png", dpi=256, bbox_inches='tight')
+plt.savefig("./heatmap-correlation-filtered.png", dpi=256, bbox_inches='tight')
 
 # detailed report of csv
 profile = ProfileReport(vechicles_csv, title="Pandas Profiling Report", vars={'num':{'low_categorical_threshold': 0}})
-profile.to_file("vehicles_csv_report.html")
-
+profile.to_file("vehicles_csv_report-filtered.html")
 
 print(vechicles_csv.columns)
 
@@ -79,37 +86,38 @@ print(vechicles_csv.columns)
 price_df = vechicles_csv.pop("price")
 vechicles_csv["price"] = price_df
 
+
 # all 14 features
 df_numpy = vechicles_csv.to_numpy()
-np.save(open("./datasets/features_14.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_14.npy", "wb"), df_numpy)
 
 # top 12 features
 features_to_drop = ["region", "paint_color"]
 new_df = vechicles_csv.drop(features_to_drop, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/features_12.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_12.npy", "wb"), df_numpy)
 
 # top 11 features
 features_to_drop = ["region", "paint_color", "state"]
 new_df = vechicles_csv.drop(features_to_drop, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/features_11.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_11.npy", "wb"), df_numpy)
 
 # top 10 features
 features_to_drop = ["region", "paint_color", "state", "type"]
 new_df = vechicles_csv.drop(features_to_drop, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/features_10.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_10.npy", "wb"), df_numpy)
 
 # top 8 features
 features_to_drop = ["region", "paint_color", "state", "type", "manufacturer", "model"]
 new_df = vechicles_csv.drop(features_to_drop, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/features_8.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_8.npy", "wb"), df_numpy)
 
 # top 5 features for baseline comparison
 baseline_features_to_consider = [ "price", "year", "manufacturer", "odometer", "transmission", "paint_color",]
 colms_to_remove = [col for col in vechicles_csv.columns if col not in baseline_features_to_consider]
 new_df = vechicles_csv.drop(colms_to_remove, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/features_5.npy", "wb"), df_numpy)
+np.save(open("./datasets/filtered/features_5.npy", "wb"), df_numpy)
