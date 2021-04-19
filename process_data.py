a57a0d57186362e29b12589d390922039d57cd4a
PriyankaMannikeri <80226560+PriyankaMannikeri@users.noreply.github.com>
2021-04-18 18:53:49 -0700

    one hot encoding

--- process_data.py
+++ process_data.py
@@ -51,11 +51,20 @@ object_cat_columns = ["region", "manufacturer", "model", "condition", "cylinders
 # df, encoding_dict = label_encoding(vechicles_csv, object_cat_columns)
 # print(encoding_dict)
 
-for col in object_cat_columns:
-    if col in vechicles_csv.columns:
-        le = LabelEncoder()
-        le.fit(list(vechicles_csv[col].astype(str).values))
-        vechicles_csv[col] = le.transform(list(vechicles_csv[col].astype(str).values))
+price_df = vechicles_csv.pop("price")
+vechicles_csv["price"] = price_df
+
+# label encoding
+# for col in object_cat_columns:
+#     if col in vechicles_csv.columns:
+#         le = LabelEncoder()
+#         le.fit(list(vechicles_csv[col].astype(str).values))
+#         vechicles_csv[col] = le.transform(list(vechicles_csv[col].astype(str).values))
+
+# one-hot encoding
+vechicles_csv = pd.get_dummies(vechicles_csv, columns=["condition", "cylinders", "fuel", "title_status", "transmission",
+                                                       "drive", "type"])
+
 
 # remove unwanted columns and duplicates
 vechicles_csv.reset_index(drop=True, inplace=True)
@@ -66,19 +75,17 @@ vechicles_csv.reset_index(drop=True, inplace=True)
 vechicles_csv.reset_index(drop=True)
 print(f"total number of rows in cleaned csv (after removing duplicates): {len(vechicles_csv)}")
 
-price_df = vechicles_csv.pop("price")
-vechicles_csv["price"] = price_df
 # correlation
 corrMatrix = vechicles_csv.corr()
 fig = plt.figure(figsize=(15, 15))
 sn.heatmap(corrMatrix, annot=True)
 plt.title("Pearson Correlation")
 # plt.show()
-plt.savefig("./heatmap-correlation-filtered.png", dpi=256, bbox_inches='tight')
+plt.savefig("./heatmap-correlation-filtered-onehot.png", dpi=256, bbox_inches='tight')
 
 # detailed report of csv
-profile = ProfileReport(vechicles_csv, title="Pandas Profiling Report", vars={'num':{'low_categorical_threshold': 0}})
-profile.to_file("vehicles_csv_report-filtered.html")
+# profile = ProfileReport(vechicles_csv, title="Pandas Profiling Report", vars={'num':{'low_categorical_threshold': 0}})
+# profile.to_file("vehicles_csv_report-filtered.html")
 
 print(vechicles_csv.columns)
 
@@ -87,36 +94,37 @@ price_df = vechicles_csv.pop("price")
 vechicles_csv["price"] = price_df
 
 # all 14 features
-df_numpy = vechicles_csv.to_numpy()
-np.save(open("./datasets/filtered/features_14.npy", "wb"), df_numpy)
-
-# top 12 features
-features_to_drop = ["region", "paint_color"]
-new_df = vechicles_csv.drop(features_to_drop, axis=1)
-df_numpy = new_df.to_numpy()
-np.save(open("./datasets/filtered/features_12.npy", "wb"), df_numpy)
+# df_numpy = vechicles_csv.to_numpy()
+# np.save(open("./datasets/filtered/features_14.npy", "wb"), df_numpy)
+#
+# # top 12 features
+# features_to_drop = ["region", "paint_color"]
+# new_df = vechicles_csv.drop(features_to_drop, axis=1)
+# df_numpy = new_df.to_numpy()
+# np.save(open("./datasets/filtered/features_12.npy", "wb"), df_numpy)
 
 # top 11 features
 features_to_drop = ["region", "paint_color", "state"]
 new_df = vechicles_csv.drop(features_to_drop, axis=1)
 df_numpy = new_df.to_numpy()
-np.save(open("./datasets/filtered/features_11.npy", "wb"), df_numpy)
+# np.save(open("./datasets/filtered/features_11.npy", "wb"), df_numpy)
+np.save(open("./datasets/onehot-encoded/features_11.npy", "wb"), df_numpy)
 
 # top 10 features
-features_to_drop = ["region", "paint_color", "state", "type"]
-new_df = vechicles_csv.drop(features_to_drop, axis=1)
-df_numpy = new_df.to_numpy()
-np.save(open("./datasets/filtered/features_10.npy", "wb"), df_numpy)
+# features_to_drop = ["region", "paint_color", "state", "type"]
+# new_df = vechicles_csv.drop(features_to_drop, axis=1)
+# df_numpy = new_df.to_numpy()
+# np.save(open("./datasets/filtered/features_10.npy", "wb"), df_numpy)
 
 # top 8 features
-features_to_drop = ["region", "paint_color", "state", "type", "manufacturer", "model"]
-new_df = vechicles_csv.drop(features_to_drop, axis=1)
-df_numpy = new_df.to_numpy()
-np.save(open("./datasets/filtered/features_8.npy", "wb"), df_numpy)
-
-# top 5 features for baseline comparison
-baseline_features_to_consider = [ "price", "year", "manufacturer", "odometer", "transmission", "paint_color",]
-colms_to_remove = [col for col in vechicles_csv.columns if col not in baseline_features_to_consider]
-new_df = vechicles_csv.drop(colms_to_remove, axis=1)
-df_numpy = new_df.to_numpy()
-np.save(open("./datasets/filtered/features_5.npy", "wb"), df_numpy)
+# features_to_drop = ["region", "paint_color", "state", "type", "manufacturer", "model"]
+# new_df = vechicles_csv.drop(features_to_drop, axis=1)
+# df_numpy = new_df.to_numpy()
+# np.save(open("./datasets/filtered/features_8.npy", "wb"), df_numpy)
+#
+# # top 5 features for baseline comparison
+# baseline_features_to_consider = [ "price", "year", "manufacturer", "odometer", "transmission", "paint_color",]
+# colms_to_remove = [col for col in vechicles_csv.columns if col not in baseline_features_to_consider]
+# new_df = vechicles_csv.drop(colms_to_remove, axis=1)
+# df_numpy = new_df.to_numpy()
+# np.save(open("./datasets/filtered/features_5.npy", "wb"), df_numpy)
